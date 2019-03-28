b75357bdb15701817d85f8945fb33958f6750837
Sam Goldstein <sam@stackery.io>
2019-03-28 16:18:15 -0700

    Add code

--- src/Function/index.js
+++ src/Function/index.js
@@ -1,5 +1,12 @@
 exports.handler = async message => {
   console.log(message);
 
-  return {};
-}
+  const response = {
+    statusCode: '200',
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: `Success\n${JSON.stringify(message, undefined, 2)}`
+  };
+  return response;
+};
